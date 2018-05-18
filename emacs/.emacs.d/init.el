1b6906d1c2638dc1dfbbdf71e6ecdca77353515c
ricky_saurav <luckyricky007@gmail.com>
2018-05-19 03:13:37 +0530

    polybar rework,emacs c++ autocomplete and snippets setup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,7 +19,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yasnippet-snippets yasnippet company-quickhelp irony company evil-nerd-commenter try sudo-edit general evil-easymotion dashboard swiper ivy avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets linum-relative helm-projectile flycheck evil))))
+    (rainbow-delimeters company-flx rainbow-mode hungry-delete fzf irony-eldoc yasnippet-snippets yasnippet company-quickhelp irony company evil-nerd-commenter try sudo-edit general evil-easymotion dashboard swiper ivy avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets linum-relative helm-projectile flycheck evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
