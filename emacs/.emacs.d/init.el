2165708ab02d180e045ba3e7bab18f3166ceab40
ricky_saurav <luckyricky007@gmail.com>
2018-05-22 03:52:55 +0530

    LSP support, basic java config using java-lsp

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,7 +9,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
-
+(setq vc-follow-symlinks t)
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org"))))
 (custom-set-variables
@@ -17,9 +17,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(jdecomp-mode t)
  '(package-selected-packages
    (quote
-    (rainbow-delimeters company-flx rainbow-mode hungry-delete fzf irony-eldoc yasnippet-snippets yasnippet company-quickhelp irony company evil-nerd-commenter try sudo-edit general evil-easymotion dashboard swiper ivy avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets linum-relative helm-projectile flycheck evil))))
+    (lsp-java jdecomp rainbow-delimeters company-flx rainbow-mode hungry-delete irony-eldoc yasnippet-snippets yasnippet company-quickhelp irony company evil-nerd-commenter try sudo-edit general evil-easymotion dashboard avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets linum-relative helm-projectile flycheck evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
