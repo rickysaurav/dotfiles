4dfa8b4e7b676c5056f7965791bc4e6fac25918f
ricky_saurav <luckyricky007@gmail.com>
2018-05-30 03:29:38 +0530

    emacs upgrade to 26.1,improved lsp-java-configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,7 +9,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
-(setq vc-follow-symlinks t)
+(setq vc-follow-symlinks nil)
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org"))))
 (custom-set-variables
@@ -20,7 +20,7 @@
  '(jdecomp-mode t)
  '(package-selected-packages
    (quote
-    (lsp-java jdecomp rainbow-delimeters company-flx rainbow-mode hungry-delete irony-eldoc yasnippet-snippets yasnippet company-quickhelp irony company evil-nerd-commenter try sudo-edit general evil-easymotion dashboard avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets linum-relative helm-projectile flycheck evil))))
+	(lsp-java jdecomp rainbow-delimeters company-flx rainbow-mode hungry-delete irony-eldoc yasnippet-snippets yasnippet company-quickhelp irony company evil-nerd-commenter try sudo-edit general evil-easymotion dashboard avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets helm-projectile flycheck evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
