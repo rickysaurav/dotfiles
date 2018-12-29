a43c55fe3e34dcbee40e0ce11101a3b9d960ab28
ricky_saurav <luckyricky007@gmail.com>
2018-12-29 23:37:05 +0530

    migrate to new lsp mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -23,7 +23,7 @@
 	("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-	(ox-reveal quelpa-use-package pyvenv pyenv pipenv exec-path-from-shell ox-ioslide indium lsp-javascript-typescript evil-magit magit helm-rg helm-ag webpaste origami hide-mode-line format-all pdf-tools ripgrep evil-surround treemacs-projectile multi-compile treemacs-evil htmlize lsp-java rainbow-delimeters company-flx rainbow-mode hungry-delete yasnippet company-quickhelp company evil-nerd-commenter try general evil-easymotion dashboard avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets helm-projectile flycheck evil))))
+	(lua-mode quelpa ox-reveal quelpa-use-package pyvenv pyenv pipenv exec-path-from-shell ox-ioslide indium lsp-javascript-typescript evil-magit magit helm-rg helm-ag webpaste origami hide-mode-line format-all pdf-tools ripgrep evil-surround treemacs-projectile multi-compile treemacs-evil htmlize lsp-java rainbow-delimeters company-flx rainbow-mode hungry-delete yasnippet company-quickhelp company evil-nerd-commenter try general evil-easymotion dashboard avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets helm-projectile flycheck evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
