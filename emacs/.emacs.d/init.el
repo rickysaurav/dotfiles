6fc841a031abf87be4323ed5f2cdca15c2a5209e
ricky_saurav <luckyricky007@gmail.com>
2019-01-29 01:46:16 +0530

    switch to straight.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,24 +9,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (setq vc-follow-symlinks nil)
+(setq gc-cons-threshold 134217728
+        gc-cons-percentage 0.5)
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org"))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#080808" "#d70000" "#67b11d" "#875f00" "#268bd2" "#af00df" "#00ffff" "#b2b2b2"])
- '(custom-safe-themes
-   (quote
-	("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages
-   (quote
-	(lsp-python-ms lua-mode quelpa ox-reveal quelpa-use-package pyvenv pyenv pipenv exec-path-from-shell ox-ioslide indium lsp-javascript-typescript evil-magit magit helm-rg helm-ag webpaste origami hide-mode-line format-all pdf-tools ripgrep evil-surround treemacs-projectile multi-compile treemacs-evil htmlize lsp-java rainbow-delimeters company-flx rainbow-mode hungry-delete yasnippet company-quickhelp company evil-nerd-commenter try general evil-easymotion dashboard avy diminish which-key use-package telephone-line spacemacs-theme smartparens rainbow-delimiters org-bullets helm-projectile flycheck evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
