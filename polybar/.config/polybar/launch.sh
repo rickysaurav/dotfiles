9e1fae833e073228c7d0b258ef5db8aeb56eb71c
ricky_saurav <luckyricky007@gmail.com>
2020-04-25 16:31:57 +0530

    Migrate to polybar themes. Credits: https://github.com/adi1090x/polybar-themes

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
 
+## Add this to your wm startup file.
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
-  done
-else
-  polybar --reload top &
-fi
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config.ini main &
