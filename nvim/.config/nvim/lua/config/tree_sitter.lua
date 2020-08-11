71d97f21031b30f1a85d36a840a67902909864f0
ricky_saurav <luckyricky007@gmail.com>
2020-08-11 23:41:04 +0530

    treesitter text objects

--- nvim/.config/nvim/lua/config/tree_sitter.lua
+++ nvim/.config/nvim/lua/config/tree_sitter.lua
@@ -13,22 +13,57 @@ function M.setup()
                 init_selection = 'gni',         -- maps in normal mode to init the node/scope selection
                 node_incremental = "gnn",       -- increment to the upper named parent
                 scope_incremental = "gns",      -- increment to the upper scope (as defined in locals.scm)
-                node_decremental = "grp",       -- decrement to the previous node
+                node_decremental = "gnp",       -- decrement to the previous node
             }
         },
         refactor = {
-            highlight_defintions = {
+            highlight_definitions = {
+                enable = true
+            },
+            highlight_current_scope = {
                 enable = true
             },
             smart_rename = {
                 enable = true,
-                smart_rename = "gR"              -- mapping to rename reference under cursor
+                keymaps = {
+                    smart_rename = "gnr"          -- mapping to rename reference under cursor
+                }
             },
             navigation = {
                 enable = true,
-                goto_definition = "gnd",          -- mapping to go to definition of symbol under cursor
-                list_definitions = "gnD"          -- mapping to list all definitions in current file
+                keymaps = {
+                    goto_definition = "gnd",      -- mapping to go to definition of symbol under cursor
+                    list_definitions = "gnD"      -- mapping to list all definitions in current file
+                }
             }
+        },
+    textobjects = { -- syntax-aware textobjects
+        enable = true,
+        disable = {},
+        keymaps = {
+            ["iL"] = { -- you can define your own textobjects directly here
+              python = "(function_definition) @function",
+              cpp = "(function_definition) @function",
+              c = "(function_definition) @function",
+              java = "(method_declaration) @function"
+            },
+            -- or you use the queries from supported languages with textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["aC"] = "@class.outer",
+            ["iC"] = "@class.inner",
+            ["ac"] = "@conditional.outer",
+            ["ic"] = "@conditional.inner",
+            ["ab"] = "@block.outer",
+            ["ib"] = "@block.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+            ["is"] = "@statement.inner",
+            ["as"] = "@statement.outer",
+            ["ad"] = "@comment.outer",
+            ["am"] = "@call.outer",
+            ["im"] = "@call.inner"
+          }
         },
         ensure_installed = {'lua','c','cpp','json','java','python','bash','typescript'} -- one of 'all', 'language', or a list of languages
     }
