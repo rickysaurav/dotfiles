c9cbf2da4da648d5892b192b9113cb4a863a0b0a
ricky_saurav <luckyricky007@gmail.com>
2020-08-21 01:16:46 +0530

    Move to tree sitter module config

--- nvim/.config/nvim/lua/config/tree_sitter.lua
+++ nvim/.config/nvim/lua/config/tree_sitter.lua
@@ -37,35 +37,67 @@ function M.setup()
                 }
             }
         },
-    textobjects = { -- syntax-aware textobjects
-        enable = true,
-        disable = {},
-        keymaps = {
-            ["iL"] = { -- you can define your own textobjects directly here
-              python = "(function_definition) @function",
-              cpp = "(function_definition) @function",
-              c = "(function_definition) @function",
-              java = "(method_declaration) @function"
+        textobjects = { -- syntax-aware textobjects
+            select = {
+                enable = true,
+                disable = {},
+                keymaps = {
+                    ["iL"] = { -- you can define your own textobjects directly here
+                      python = "(function_definition) @function",
+                      cpp = "(function_definition) @function",
+                      c = "(function_definition) @function",
+                      java = "(method_declaration) @function"
+                    },
+                    -- or you use the queries from supported languages with textobjects.scm
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["aC"] = "@class.outer",
+                    ["iC"] = "@class.inner",
+                    ["ac"] = "@conditional.outer",
+                    ["ic"] = "@conditional.inner",
+                    ["ab"] = "@block.outer",
+                    ["ib"] = "@block.inner",
+                    ["al"] = "@loop.outer",
+                    ["il"] = "@loop.inner",
+                    ["is"] = "@statement.inner",
+                    ["as"] = "@statement.outer",
+                    ["ad"] = "@comment.outer",
+                    ["am"] = "@call.outer",
+                    ["im"] = "@call.inner"
+                }
             },
-            -- or you use the queries from supported languages with textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["aC"] = "@class.outer",
-            ["iC"] = "@class.inner",
-            ["ac"] = "@conditional.outer",
-            ["ic"] = "@conditional.inner",
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-            ["al"] = "@loop.outer",
-            ["il"] = "@loop.inner",
-            ["is"] = "@statement.inner",
-            ["as"] = "@statement.outer",
-            ["ad"] = "@comment.outer",
-            ["am"] = "@call.outer",
-            ["im"] = "@call.inner"
-          }
+         -- swap parameters (keymap -> textobject query)
+            swap  =  {
+                 enable = true,
+                 swap_next = {
+                     ["<leader>N"] = "@parameter.inner",
+                 },
+                 swap_previous = {
+                     ["<leader>P"] = "@parameter.inner",
+                 },
+             },
+          -- set mappings to go to start/end of adjacent textobjects (keymap -> textobject query)
+            move = {
+                  enable = true,
+                  goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                  },
+                  goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                  },
+                  goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                  },
+                  goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                  },
+            },
         },
-        ensure_installed = {'lua','c','cpp','json','java','python','bash','typescript'} -- one of 'all', 'language', or a list of languages
+      ensure_installed = {'lua','c','cpp','json','java','python','bash','typescript'} -- one of 'all', 'language', or a list of languages
     }
 end
 
