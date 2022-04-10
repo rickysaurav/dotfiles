443d044c76510a9943e59be51b890fb037de3135
ricky_saurav <luckyricky007@gmail.com>
2022-04-10 13:28:18 +0530

    update ranger

--- modules/home-manager/dots/ranger/default.nix
+++ modules/home-manager/dots/ranger/default.nix
@@ -1,7 +1,9 @@
 {config,pkgs,...}:{
- imports = [../../programs/ranger.nix];
- programs.ranger = {
-     enable = true;
-     imagePreviewMethod = "iterm2";
- };
+home.packages = with pkgs; [
+    ranger
+];
+xdg.configFile."ranger/commands.py".source = ./commands.py;
+xdg.configFile."ranger/rc.conf".source = ./rc.conf;
+xdg.configFile."ranger/devicons.py".source = ./devicons.py;
+xdg.configFile."ranger/plugins/devicons_linemode.py".source = ./plugins/devicons_linemode.py;
 }
